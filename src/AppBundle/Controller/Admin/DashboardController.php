4c5e03c2d71754c99fa5c26da2b1521b2c09cbb8
joel <jrakotoarison@bocasay.fr>
2016-08-10 18:23:44 +0300

    correction merge

--- src/AppBundle/Controller/Admin/DashboardController.php
+++ src/AppBundle/Controller/Admin/DashboardController.php
@@ -1,10 +1,6 @@
 <?php
 
 namespace AppBundle\Controller\Admin;
-<<<<<<< HEAD:src/AppBundle/Controller/Admin/DashboardController.php
-
-=======
->>>>>>> origin/feature/feat-gestion-conges-employee:src/AppBundle/Controller/admin/DashboardController.php
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
